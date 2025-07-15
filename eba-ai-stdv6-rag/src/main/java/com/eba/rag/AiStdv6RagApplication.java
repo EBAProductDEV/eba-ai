0dacf200f31ada436218a695001b27ce1e7ba109
zhangxubo <102790028+2506030321@users.noreply.github.com>
2025-07-15 19:21:50 +0800

    创建基于RAG实现本地代码知识问答系统微服务

---
+++ eba-ai-stdv6-rag/src/main/java/com/eba/rag/AiStdv6RagApplication.java
@@ -0,0 +1,13 @@
+package com.eba.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiStdv6RagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiStdv6RagApplication.class, args);
+    }
+
+}
---
+++ eba-ai-stdv6-rag/src/test/java/com/eba/rag/EbaAiStdv6RagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eba.rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbaAiStdv6RagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
