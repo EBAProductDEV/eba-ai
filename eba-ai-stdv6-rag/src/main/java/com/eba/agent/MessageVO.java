6563deb66510f7d10e619cb1545c9fed90fb447e
zhangxubo <102790028+2506030321@users.noreply.github.com>
2026-01-04 21:03:25 +0800

    简单调通与后端实现流式输出

---
+++ eba-ai-stdv6-rag/src/main/java/com/eba/agent/MessageVO.java
@@ -0,0 +1,13 @@
+package com.eba.agent;
+
+import lombok.Data;
+
+/**
+ * @Author: 粪豆儿
+ * @CreateTime: 2026-01-03 22:42:17
+ * @Desc:
+ */
+@Data
+public class MessageVO {
+  private String question;
+}
