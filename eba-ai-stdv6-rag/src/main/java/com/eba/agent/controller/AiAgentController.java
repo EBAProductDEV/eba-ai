c2683bedd2942aae520dcb9b9fd12efb4108ee75
zhangxubo <102790028+2506030321@users.noreply.github.com>
2026-01-04 21:07:36 +0800

    简单调通与后端实现流式输出

--- eba-ai-stdv6-rag/src/main/java/com/eba/agent/controller/AiAgentController.java
+++ eba-ai-stdv6-rag/src/main/java/com/eba/agent/controller/AiAgentController.java
@@ -1,6 +1,7 @@
 package com.eba.agent.controller;
 
 import com.alibaba.cloud.ai.graph.agent.ReactAgent;
+import com.eba.agent.MessageVO;
 import jakarta.annotation.Resource;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.Message;
@@ -9,10 +10,7 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
@@ -29,7 +27,7 @@ public class AiAgentController {
 
     @Resource(name = "dashScopeQwenModel")
     private ChatModel chatModel;
-    @Resource(name = "ollamaQwenClient")
+    @Resource(name = "dashScopeQwenClient")
     private ChatClient chatClient;
     @Resource
     @Qualifier("dashScopeQwenAgent")
