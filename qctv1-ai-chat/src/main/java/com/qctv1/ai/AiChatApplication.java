32259236be6be8655eca6fcb459ceb7e31fd7108
zhangxubo <102790028+2506030321@users.noreply.github.com>
2026-04-13 11:22:38 +0800

    增加网关，路由转发功能

--- qctv1-ai-chat/src/main/java/com/qctv1/ai/AiChatApplication.java
+++ qctv1-ai-chat/src/main/java/com/qctv1/ai/AiChatApplication.java
@@ -2,13 +2,13 @@ package com.qctv1.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AiChatApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(AiChatApplication.class, args);
     }
-
 }
--- qctv1-ai-rag/src/main/java/com/qctv1/AiStdv6RagApplication.java
+++ qctv1-ai-rag/src/main/java/com/qctv1/AiStdv6RagApplication.java
@@ -3,14 +3,14 @@ package com.qctv1;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.qctv1.rag.mapper")
 public class AiStdv6RagApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AiStdv6RagApplication.class, args);
     }
-
 }
